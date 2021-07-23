15cd19bf2e9c543ff9f86087401cd300f3d2aee3
manchivarusanthosh <manchivarusanthoshkumar@gmail.com>
2021-07-23 17:54:11 +0530

    styles

--- src/components/Employee/LeaveApplicationEmpForm.jsx
+++ src/components/Employee/LeaveApplicationEmpForm.jsx
@@ -3,83 +3,98 @@ import "./LeaveApplicationEmpForm.css";
 import { Form, Button, Col, Row } from "react-bootstrap";
 import axios from "axios";
 
-function LeaveApplicationEmpForm(props)  {
+function LeaveApplicationEmpForm(props) {
     // state = {
     // };
     // componentWillMount() {
 
     // }
-    
-        return (
-            <div>
-                <h2 id="role-form-title">Add LeaveApplicationEmp Details</h2>
-                <div id="role-form-outer-div"><Form id="form" onSubmit={props.onLeaveApplicationEmpSubmit}>
-                    <Form.Group as={Row} >
-                        <Form.Label column sm={2}>
-                            Leave Type
-                        </Form.Label>
-                        <Col sm={10} className="form-input">
-                            <Form.Control as="select" required>
-                                <option value="" disabled selected>
-                                    Select your option
-                                </option>
-                                <option value="Sick Leave">Sick Leave</option>
-                                <option value="Casual Leave">Casual Leave</option>
-                                <option value="Privilege Leave">Privilege Leave</option>
-                            </Form.Control>
-                        </Col>
-                    </Form.Group>
-                    <Form.Group as={Row}>
-                        <Form.Label column sm={2}>
-                            FromDate
-                        </Form.Label>
-                        <Col sm={10} className="form-input">
-                            <Form.Control type="date" required />
-                        </Col>
-                    </Form.Group>
-                    <Form.Group as={Row}>
-                        <Form.Label column sm={2}>
-                            ToDate
-                        </Form.Label>
-                        <Col sm={10} className="form-input">
-                            <Form.Control type="date" placeholder="ToDate" required />
-                        </Col>
-                    </Form.Group>
-                    <Form.Group as={Row}>
-                        <Form.Label column sm={2}>
-                            Reason for leave
-                        </Form.Label>
-                        <Col sm={10} className="form-input">
-                            <Form.Control type="Text" placeholder="Reason for leave" required />
-                        </Col>
-                    </Form.Group>
 
-                    <Form.Group as={Row} >
-                        <Form.Label column sm={2}>
-                            Leave Status
-                        </Form.Label>
-                        <Col sm={10} className="form-input">
-                            <Form.Control as="select" required>
-                                <option value="1" selected>Pending</option>
-                            </Form.Control>
-                        </Col>
-                    </Form.Group>
-                <div className="submit-and-cancel-button">       
-                    <Form.Group as={Row} id="form-submit-button">
-                        <Col sm={{ span: 10, offset: 2 }}>
-                            <Button type="submit">Submit</Button>
-                        </Col>
-                    </Form.Group>
-                    <Form.Group as={Row} id="form-cancel-button">
-                        <Col sm={{ span: 10, offset: 2 }} id="form-cancel-button-inner">
-                            <Button type="reset" className="cancel-button" onClick={props.onFormClose}>Cancel</Button>
-                        </Col>
-                    </Form.Group>
-                    </div>
-                </Form></div>
+    return (
+        <div>
+            <div className="form-container">
+                <h2 id="role-form-title">Add LeaveApplicationEmp Details</h2>
+                <div id="role-form-outer-div">
+                    <Form id="form" onSubmit={props.onLeaveApplicationEmpSubmit}>
+                        <div className='label-option-container'>
+                            <Form.Group as={Row} >
+                                <Form.Label column sm={2}>
+                                    Leave Type
+                                </Form.Label>
+                                <Col sm={10} className="form-input">
+                                    <Form.Control as="select" required>
+                                        <option value="" disabled selected>
+                                            Select your option
+                                        </option>
+                                        <option value="Sick Leave">Sick Leave</option>
+                                        <option value="Casual Leave">Casual Leave</option>
+                                        <option value="Privilege Leave">Privilege Leave</option>
+                                    </Form.Control>
+                                </Col>
+
+                            </Form.Group>
+                        </div>
+
+                        <div className='label-option-container'>
+                            <Form.Group as={Row}>
+                                <Form.Label column sm={2}>
+                                    FromDate
+                                </Form.Label>
+                                <Col sm={10} className="form-input">
+                                    <Form.Control type="date" required />
+                                </Col>
+                            </Form.Group>
+                            </div>
+                            <div className='label-option-container'>
+                            <Form.Group as={Row}>
+                                <Form.Label column sm={2}>
+                                    ToDate
+                                </Form.Label>
+                                <Col sm={10} className="form-input">
+                                    <Form.Control type="date" placeholder="ToDate" required />
+                                </Col>
+                            </Form.Group>
+                        </div>
+                        <div className='label-option-container'>
+                            <Form.Group as={Row}>
+                                <Form.Label column sm={2}>
+                                    Reason for leave
+                                </Form.Label>
+                                <Col sm={10} className="form-input">
+                                    <Form.Control type="Text" placeholder="Reason for leave" required />
+                                </Col>
+                            </Form.Group>
+                        </div>
+                        <div className='label-option-container'>
+                            <Form.Group as={Row} >
+                                <Form.Label column sm={2}>
+                                    Leave Status
+                                </Form.Label>
+                                <Col sm={10} className="form-input">
+                                    <Form.Control as="select" required>
+                                        <option value="1" selected>Pending</option>
+                                    </Form.Control>
+                                </Col>
+                            </Form.Group>
+                        </div>
+
+                        <div className="submit-and-cancel-button">
+                            <Form.Group as={Row} id="form-submit-button">
+                                <Col sm={{ span: 10, offset: 2 }}>
+                                    <Button type="submit">Submit</Button>
+                                </Col>
+                            </Form.Group>
+                            <Form.Group as={Row} id="form-cancel-button">
+                                <Col sm={{ span: 10, offset: 2 }} id="form-cancel-button-inner">
+                                    <Button type="reset" className="cancel-button" onClick={props.onFormClose}>Cancel</Button>
+                                </Col>
+                            </Form.Group>
+                        </div>
+                    </Form></div>
             </div>
-        );
-    
+        </div>
+    );
+
 }
 
 export default LeaveApplicationEmpForm;
